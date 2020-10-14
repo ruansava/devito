481d3106e0aea73ece101dba65550ac8d4c85fec
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-10-14 18:37:06 +0200

    aaaaaa

--- devito/passes/clusters/streaming.py
+++ devito/passes/clusters/streaming.py
@@ -11,22 +11,71 @@ from devito.types import (Array, CustomDimension, Eq, Lock, WaitLock, SetLock, U
                           WaitThread, WithThread, SyncData, DeleteData, STDThread,
                           ModuloDimension)
 
-__all__ = ['Asynchrony', 'Prefetching']
+__all__ = ['Tasker', 'Prefetching']
 
 
-class Asynchrony(Queue):
+class Tasker(Queue):
 
     """
-    Create asynchronous Clusters. This boils down to tagging Clusters
-    with suitable SyncOps, such as WaitLock, WithThread, etc.
+    Create asynchronous Clusters, or "tasks".
 
     Parameters
     ----------
     key : callable, optional
-        A Cluster `c` is made asynchronous only if `key(c)` returns True.
+        A Cluster `c` becomes asynchronous iff `key(c)` returns True.
+
+    Notes
+    -----
+    From an implementation viewpoint, an asynchronous Cluster is a Cluster
+    with attached suitable SyncOps, such as WaitLock, WithThread, etc.
     """
 
-    pass
+    def __init__(self, key):
+        assert callable(key)
+        self.key = key
+        super().__init__()
+
+    @timed_pass(name='tasker')
+    def process(self, clusters):
+        return super().process(clusters)
+
+    def callback(self, clusters, prefix):
+        if not prefix:
+            return clusters
+
+        d = prefix[-1].dim
+
+        if not all(c.properties[d] >= {SEQUENTIAL, AFFINE} for c in clusters):
+            return clusters
+
+        locks = {}
+        mapper = defaultdict(list)
+        for c0 in clusters:
+            if not self.key(c0):
+                continue
+
+            # Prevent future writes to any of the Functions read by `c0`
+            # by waiting on a lock
+            may_require_lock = {i for i in c0.scope.reads if i.is_AbstractFunction}
+            protected_indices = set()
+            for c1 in clusters:
+                if c0 is c1:
+                    continue
+
+                require_lock = may_require_lock & set(c1.scope.writes)
+                for i in require_lock:
+                    lock = locks.setdefault(i, Lock(name='lock%d' % len(locks),
+                                                    dimensions=i.indices[d]))
+                    for w in c1.scope.writes[i]:
+                        index = w[d]
+                        if index in protected_indices:
+                            # A `wait` already added in one of the previous Clusters
+                            continue
+
+                        mapper[c1].append(WaitLock(lock[index]))
+                        protected_indices.add(index)
+
+        return clusters
 
 #    # Create a lock to avoid race conditions should the buffer be accessed
 #    # in read and write mode by two e.g. two different threads
@@ -74,3 +123,56 @@ class Prefetching(Queue):
     pass
 
 
+class AccessV(object):
+
+    """
+    A simple data structure capturing the accesses performed by a given Function.
+
+    Parameters
+    ----------
+    function : Function
+        The target Function.
+    readby : list of Cluster
+        All Clusters accessing `function` in read mode.
+    writtenby : list of Cluster
+        All Clusters accessing `function` in write mode.
+    """
+
+    def __init__(self, function, readby, writtenby):
+        self.function = function
+        self.readby = as_tuple(readby)
+        self.writtenby = as_tuple(writtenby)
+
+    @cached_property
+    def allclusters(self):
+        return filter_ordered(self.readby + self.writtenby)
+
+    @cached_property
+    def lastwrite(self):
+        try:
+            return self.writtenby[-1]
+        except IndexError:
+            return None
+
+    @cached_property
+    def accesses(self):
+        return tuple(chain(*[c.scope.getreads(self.function) +
+                             c.scope.getwrites(self.function) for c in self.allclusters]))
+
+
+class AccessMapper(OrderedDict):
+
+    def __init__(self, clusters, key):
+        writtenby = DefaultOrderedDict(list)
+        readby = DefaultOrderedDict(list)
+        for c in clusters:
+            for f in c.scope.writes:
+                if key(f):
+                    writtenby[f].append(c)
+            for f in c.scope.reads:
+                if key(f):
+                    readby[f].append(c)
+
+        self.functions = functions = filter_ordered(list(writtenby) + list(readby))
+
+        super().__init__([(f, AccessV(f, readby[f], writtenby[f])) for f in functions])
