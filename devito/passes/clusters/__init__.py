16f92e5503bb5c17c63403aebd96830c7e10ddc6
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-10-14 18:37:06 +0200

    aaaaaa

--- devito/passes/clusters/__init__.py
+++ devito/passes/clusters/__init__.py
@@ -3,5 +3,5 @@ from .aliases import *  # noqa
 from .cse import *  # noqa
 from .factorization import *  # noqa
 from .blocking import *  # noqa
-from .streaming import *  # noqa
+from .tasking import *  # noqa
 from .misc import *  # noqa
