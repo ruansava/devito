130f9b864a353ce3a54bd42aedcf040185ba3703
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-10-15 12:04:22 +0200

    aaaaa

--- devito/passes/clusters/asynchrony.py
+++ devito/passes/clusters/asynchrony.py
@@ -8,8 +8,7 @@ from devito.ir.support import AFFINE, SEQUENTIAL, Scope
 from devito.symbolics import uxreplace
 from devito.tools import DefaultOrderedDict, as_tuple, filter_ordered, flatten, timed_pass
 from devito.types import (Array, CustomDimension, Eq, Lock, WaitLock, SetLock, UnsetLock,
-                          WaitThread, WithThread, SyncData, DeleteData, STDThread,
-                          ModuloDimension)
+                          CopyData, DeleteData, ModuloDimension)
 
 __all__ = ['Tasker', 'Prefetching']
 
@@ -49,63 +48,69 @@ class Tasker(Queue):
             return clusters
 
         locks = {}
-        mapper = defaultdict(list)
+        waits = defaultdict(list)
+        tasks = defaultdict(list)
         for c0 in clusters:
             if not self.key(c0):
                 continue
 
-            # Prevent future writes to any of the Functions read by `c0`
-            # by waiting on a lock
+            # Prevent future writes to interfere with a task by waiting on a lock
             may_require_lock = {i for i in c0.scope.reads if i.is_AbstractFunction}
-            protected_indices = set()
+            protected = defaultdict(set)
             for c1 in clusters:
-                if c0 is c1:
-                    continue
+                offset = int(clusters.index(c1) <= clusters.index(c0))
 
-                require_lock = may_require_lock & set(c1.scope.writes)
-                for i in require_lock:
-                    lock = locks.setdefault(i, Lock(name='lock%d' % len(locks),
-                                                    dimensions=i.indices[d]))
-                    for w in c1.scope.writes[i]:
-                        index = w[d]
-                        if index in protected_indices:
-                            # A `wait` already added in one of the previous Clusters
+                for f in may_require_lock:
+                    try:
+                        writes = c1.scope.writes[f]
+                    except KeyError:
+                        # No read-write dependency, ignore
+                        continue
+
+                    try:
+                        ld = f.indices[d]
+                    except KeyError:
+                        # Would degenerate to a scalar, but we rather use an Array
+                        # of size 1 for simplicity
+                        ld = CustomDimension(name='ld', symbolic_size=1)
+                    lock = locks.setdefault(f, Lock(name='lock%d' % len(locks),
+                                                    dimensions=ld))
+
+                    for w in writes:
+                        try:
+                            index = w[d]
+                            logical_index = index + offset
+                        except TypeError:
+                            assert ld.symbolic_size == 1
+                            index = 0
+                            logical_index = 0
+
+                        if logical_index in protected[f]:
                             continue
 
-                        mapper[c1].append(WaitLock(lock[index]))
-                        protected_indices.add(index)
+                        waits[c1].append(WaitLock(lock[index]))
+                        protected[f].add(logical_index)
 
-        return clusters
+            # Taskify `c0`
+            acquired = []
+            copyrelease = []
+            for f in protected:
+                lock = locks[f]
 
-#    # Create a lock to avoid race conditions should the buffer be accessed
-#    # in read and write mode by two e.g. two different threads
-#    self.lock = Lock(name='lock%d' % n, dimensions=bd)
+                try:
+                    indices = sorted({r[d] for r in c0.scope.reads[f]})
+                except TypeError:
+                    assert lock.size == 1
+                    indices = [0]
 
-#    # Construct the sync-data operation
-#    sync_data.append(b.buffer[indices])
-#
-#    # Construct the synchronization operations
-#    thread = STDThread(name='thread%d' % (len(processed) - len(clusters)))
-#    sync_ops = (
-#        [WaitThread(thread)] +
-#        [SetLock(b.lock[indices[b.index]]) for b in buffereds] +
-#        [WithThread(thread)] +
-#        sync_data +
-#        [UnsetLock(b.lock[indices[b.index]]) for b in buffereds] +
-#            SyncData()
-#         ))]
-#    )
-#    sync_ops 
-#   syncs = defaultdict(list)
-#   for b in buffereds:
-#       from IPython import embed; embed()
-#       syncs[b.dim].extend([
-#            WaitThread(thread),
-#            SetLock(b.lock[indices[b.index]]),
-#            WithThread(thread, [
-#                SyncData()
-#            ])
-#        ])
+                acquired.extend([SetLock(lock[i]) for i in indices])
+                copyrelease.extend([CopyData(f, {d: i}) for i in indices])
+                copyrelease.extend([UnsetLock(lock[i]) for i in indices])
+            tasks[c0] = acquired + copyrelease
+
+        processed = [c.rebuild(syncs={d: waits[c] + tasks[c]}) for c in clusters]
+
+        return processed
 
 
 class Prefetching(Queue):
@@ -121,58 +126,3 @@ class Prefetching(Queue):
     """
 
     pass
-
-
-class AccessV(object):
-
-    """
-    A simple data structure capturing the accesses performed by a given Function.
-
-    Parameters
-    ----------
-    function : Function
-        The target Function.
-    readby : list of Cluster
-        All Clusters accessing `function` in read mode.
-    writtenby : list of Cluster
-        All Clusters accessing `function` in write mode.
-    """
-
-    def __init__(self, function, readby, writtenby):
-        self.function = function
-        self.readby = as_tuple(readby)
-        self.writtenby = as_tuple(writtenby)
-
-    @cached_property
-    def allclusters(self):
-        return filter_ordered(self.readby + self.writtenby)
-
-    @cached_property
-    def lastwrite(self):
-        try:
-            return self.writtenby[-1]
-        except IndexError:
-            return None
-
-    @cached_property
-    def accesses(self):
-        return tuple(chain(*[c.scope.getreads(self.function) +
-                             c.scope.getwrites(self.function) for c in self.allclusters]))
-
-
-class AccessMapper(OrderedDict):
-
-    def __init__(self, clusters, key):
-        writtenby = DefaultOrderedDict(list)
-        readby = DefaultOrderedDict(list)
-        for c in clusters:
-            for f in c.scope.writes:
-                if key(f):
-                    writtenby[f].append(c)
-            for f in c.scope.reads:
-                if key(f):
-                    readby[f].append(c)
-
-        self.functions = functions = filter_ordered(list(writtenby) + list(readby))
-
-        super().__init__([(f, AccessV(f, readby[f], writtenby[f])) for f in functions])
